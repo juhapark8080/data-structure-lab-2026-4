d24e7787095ee854226cf158dc5e25ca2649bd92
juhapark8080 <juha8080@gmail.com>
2026-05-15 10:50:30 +0900

    graph

--- Lec10_Graph/Lec10_Graph/main.cpp
+++ Lec10_Graph/Lec10_Graph/main.cpp
@@ -3,13 +3,19 @@ int main()
 {
 	SrchAMGraph g;
 
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 6; i++)
 		g.insertVertex('A' + i);
 	g.insertEdge(0, 1);
 	g.insertEdge(0, 3);
+	g.insertEdge(0, 5);
 	g.insertEdge(1, 2);
 	g.insertEdge(1, 3);
+	g.insertEdge(1, 4);
 	g.insertEdge(2, 3);
+	g.insertEdge(3, 4);
+	g.insertEdge(3, 5);
+	g.insertEdge(4, 5);
+
 	g.display();
 	FILE* fp;
 	fopen_s(&fp, "../text_out.txt", "w");
