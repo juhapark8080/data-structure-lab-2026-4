baf57e89f0c1482ed7c5af5a75044a43c535bfaf
juhapark8080 <juha8080@gmail.com>
2026-06-19 14:18:31 +0900

    Final

--- Lec10_Graph/Lec10_Graph/graph_main.cpp
+++ Lec10_Graph/Lec10_Graph/graph_main.cpp
@@ -1,14 +1,46 @@
-#include "WGraphMST.h"
+#include "WGraphPrim.h"
+#include "WGraphDijkstra.h"
 
 int main()
 {
-    WGraphMST g;
+    int menu;
 
-    char filename[] = "graph.txt";
-    g.load(filename);
+    printf("1. Prim MST\n");
+    printf("2. Dijkstra Shortest Path\n");
+    printf("선택: ");
+    scanf_s("%d", &menu);
 
-    printf("MST By Kruskal's Algorithm\n");
-    g.Kruskal();
+    switch (menu) {
+    case 1:
+    {
+        WGraphPrim g;
+        char filename[] = "graph.txt";
+
+        g.load(filename);
+
+        printf("MST By Prim's Algorithm\n");
+        g.Prim(0);
+
+        break;
+    }
+
+    case 2:
+    {
+        WGraphDijkstra g;
+        char filename[] = "graph_sp.txt";
+
+        g.load(filename);
+
+        printf("Shortest Path By Dijkstra Algorithm\n");
+        g.ShortestPath(0);
+
+        break;
+    }
+
+    default:
+        printf("잘못된 선택입니다.\n");
+        break;
+    }
 
     return 0;
 }
